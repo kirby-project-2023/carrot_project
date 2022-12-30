4ea3a3985a304311c4424e61d4e7f0f260d9ffdb
EunjiLee1997 <eunjilee1111@gmail>
2022-12-30 20:18:12 +0900

    feat: App.js에 Index 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import styled from 'styled-components';
 import GlobalStyle from './styles/GlobalStyle';
 import { Fragment } from 'react';
 import Guide from './components/Guide'
+import Index from './components/Index'
 
 function App() {
   return (
     <Fragment>
-      <GlobalStyle /> 
-      <Guide/>
+      <Index /> 
     </Fragment>
   );
 }
