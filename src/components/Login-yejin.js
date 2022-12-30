ae510a1b851df9bef1fdf390784e5c8673f06234
yjyaang <topbuty97@gmail.com>
2022-12-30 20:13:54 +0900

    feat: 로그인 기능 구현-yejin

--- src/components/Login-yejin.js
+++ src/components/Login-yejin.js
@@ -1,12 +1,19 @@
 import React, { useState } from 'react'
 {/*import user 더미데이터 */ }
 {/*react-router-dom을 써야 하나? */ }
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
+const user = [{
+    id: 'aaa',
+    pw: '1234'
+}];
 
 const Login = () => {
     const [inputId, setInputId] = useState('');
     const [inputPw, setInputPw] = useState('');
+    {/*버튼 누른 상태? */}
+
+    const history = useHistory();
 
     const handleInputId = (event) => {
         setInputId(event.target.value);
@@ -20,18 +27,18 @@ const Login = () => {
     {/*로그인 버튼 onClick에 들어갈 함수
     -> if 아이디가 유저아이디 중에 있고 패스워드가 맞으면 마이페이지로 넘어가기
     아니면 alert*/}
-    const loginBtn = () => {
-        if (inputId === 'yjyaang' && inputPw === '1234') {
-            {/*마이페이지로 넘어가기 */ }
-            {/*history.push(/mypage) */}
+    const loginBtn = (e) => {
+        e.preventDefault();
+        {/*inputId, inputPw -> 더미데이터 비교해서 있으면 마이페이지로 넘어가고
+        아니면 알림창 띄우기*/}
+        const arr = user.filter(el => el.id === inputId && el.pw === inputPw);
+        if(arr.length === 1) {
+            history.push('/mypage');
         }
-        else {
-            alert('회원정보가 일치하지 않습니다')
+        else if(arr.length === 0) {
+            alert('회원정보가 일치하지 않습니다');
         }
     }
-
-    {/*회원가입 버튼 onClick에 들어갈 함수
-    -> Auth.js로 이동 */}
 
     return (
         <div>
@@ -43,13 +50,9 @@ const Login = () => {
 
             {/*버튼 -> 로그인 / 회원가입 버튼*/}
             <button onClick={loginBtn}>로그인</button>
-            {/*로그인 버튼 성공 ->
-            버튼 누르면 input의 value를 유저 더미데이터랑 비교해서
-            일치하는 경우 Mypage.js로 이동*/}
-            {/*로그인 버튼 실패 -> alert(회원정보가 일치하지 않습니다)*/}
-
             {/*회원가입 버튼 ->*/}
             <Link to="/auth"><button>회원가입</button></Link>
+            {/*삼항연산자 -> 어떤 상태 속성이 true 됐을 때만 회원가입 버튼이 뜨게 하자 */}
             {/* close 버튼 -> Index.js로 이동
             <button>닫기</button> */}
         </div>
