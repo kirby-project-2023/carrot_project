d3dc1de9c4ed18370865cb8cb27a12a616399da2
hajiung25 <hajiung25@gmail.com>
2023-01-06 20:24:19 +0900

    feat:

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const MainLogo = styled.img`
+
+`
+
+const Header = () => {
+    return (
+        <Header>
+
+        </Header>
+    )
+};
+
+
+export default Header;
