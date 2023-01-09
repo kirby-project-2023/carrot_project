3b09a4c15de3bbf8d2f10162acd13dcdfc54e5c8
EunjiLee1997 <eunjilee1111@gmail>
2023-01-09 21:08:53 +0900

    Feat: Footer 컴포넌트 생성

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const CopyRights = styled.footer`
+
+`
+
+function Footer() {
+  return (
+    <CopyRights>Copyright 2023. kirby_project all rights reserved. <br/> Contributors</CopyRights>
+  )
+}
+
+export default Footer
