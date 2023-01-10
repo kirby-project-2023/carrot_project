365da73a0aa34415182bba216305adddcdec0d2d
yjyaang <topbuty97@gmail.com>
2023-01-10 21:56:25 +0900

    feat: Mypage 버튼 구현

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,7 +9,6 @@ const LoginForm = styled.form`
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    
 `;
 const LoginBox = styled.div`
     position: relative;
@@ -58,11 +57,11 @@ const Button = styled.button`
     margin-top: var(--gap-sm);
 `
 
-const Login = ({ setNickname, userData, setUserInfo }) => {
+const Login = ({ userData, setUserInfo }) => {
     const [inputId, setInputId] = useState('');
     const [inputPw, setInputPw] = useState('');
-    {/*버튼 누른 상태? */ }
-    {/*나한테 왜 이래애애애애앵*/}
+    // 버튼 누른 상태?
+    // 나한테 왜 이래애애애애앵
 
     const navigate = useNavigate();
 
@@ -74,26 +73,24 @@ const Login = ({ setNickname, userData, setUserInfo }) => {
         setInputPw(event.target.value);
     };
 
-    {/*로그인 버튼 onClick에 들어갈 함수
-    -> if 아이디가 유저아이디 중에 있고 패스워드가 맞으면 마이페이지로 넘어가기
-    아니면 alert*/}
+    // 로그인 버튼 onClick에 들어갈 함수
+    // -> if 아이디가 유저아이디 중에 있고 패스워드가 맞으면 마이페이지로 넘어가기
+    // 아니면 alert
     const loginBtn = (e) => {
         e.preventDefault();
-        {/*inputId, inputPw -> 더미데이터 비교해서 있으면 마이페이지로 넘어가고
-        아니면 알림창 띄우기*/}
+        // inputId, inputPw -> 더미데이터 비교해서 있으면 마이페이지로 넘어가고
+        // 아니면 알림창 띄우기
         const arr = userData.filter(el => el.id === inputId && el.pw === inputPw);
         if (arr.length === 1) {
             navigate('/mypage');
-            {/*로그인 된 유저를 상태관리 추가하기 */}
+            // 로그인 된 유저를 상태관리 추가하기
             setUserInfo(arr)
-            setNickname(arr[0].field)
-            {/*로그인하면 마이페이지에도 '***네 당근밭'처럼 로그인 정보가 들어있어야 함
-            로그인 버튼 누르는 순간에 이걸 상태로 가지고 마이페이지에 넘겨줘야 마이페이지에서 닉네임을 쓸 수 있을 것 같음
-            그래서 로그인 js와 마이페이지 js의 상위 컴포넌트인 App에서 상태관리를 해줘야 할 것 같음
-            App에서 useState로 nickname, setNickname 설정해서(초기값 null?빈문자열?) 로그인에는 setNickname을 props로 넘겨주고
-            마이페이지에는 nickName를 넘김
-            마이페이지 -> 닉네임, 편지 넘겨주면 어떨지
-            */}
+            // 로그인하면 마이페이지에도 '***네 당근밭'처럼 로그인 정보가 들어있어야 함
+            // 로그인 버튼 누르는 순간에 이걸 상태로 가지고 마이페이지에 넘겨줘야 마이페이지에서 닉네임을 쓸 수 있을 것 같음
+            // 그래서 로그인 js와 마이페이지 js의 상위 컴포넌트인 App에서 상태관리를 해줘야 할 것 같음
+            // App에서 useState로 nickname, setNickname 설정해서(초기값 null?빈문자열?) 로그인에는 setNickname을 props로 넘겨주고
+            // 마이페이지에는 nickName를 넘김
+            // 마이페이지 -> 닉네임, 편지 넘겨주면 어떨지
         }
         else if (arr.length === 0) {
             alert('회원정보가 일치하지 않습니다');
