cb7ab3041e9238000dfce5201fc5a7186891faeb
EunjiLee1997 <eunjilee1111@gmail>
2023-01-17 20:39:45 +0900

    feat: FONT-AWESOME 설치 및 Header.js 로그아웃 기능 구현

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,8 @@ import Logo1 from '../img/Logo1.png'
 import Logo2 from '../img/Logo2.png'
 import Logo3 from '../img/Logo3.png'
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 
 const MainLogo = styled.img.attrs(props => ({
   src: props.changeImg
@@ -32,40 +34,52 @@ const Title = styled.h1`
   text-align: center;
 `;
 
-const NickName = styled.div`
+const Button = styled.button`
   width: 80px;
   height: 80px;
   display: flex;
   flex-direction: column;
   text-align: center;
+  align-items: center;
   justify-content: center;
   font-size: var(--fz-big);
+  color: var(--white);
+  :hover{
+    cursor: pointer;
+  }
 `
 
 const Header = ({ userInfo }) => {
   const [changeImg, setChangeImg] = useState(LogoDefault);
+
   useEffect(() => {
     if (userInfo.length === 0 || userInfo[0].contentLst === undefined) {
       setChangeImg(LogoDefault)
     }
-    else if (userInfo[0].contentLst.length <= 3) {
+    else if (userInfo[0].contentLst.length <= 4) {
       setChangeImg(Logo1)
-    } else if (userInfo[0].contentLst.length <= 6) {
+    } else if (userInfo[0].contentLst.length <= 8) {
       setChangeImg(Logo2)
-    } else if (userInfo[0].contentLst.length <= 9) {
+    } else if (userInfo[0].contentLst.length <= 12) {
       setChangeImg(Logo3)
     }
-  }, [userInfo])
-  console.log(userInfo)
+  }, [userInfo]);
+
+  const logoutHandler = () => {
+    localStorage.removeItem('userInfo');
+    window.location.href = 'http://localhost:3000/';
+  }
+
   return (
-
     <HeaderContainer>
-      <Link to="/"><MainLogo changeImg={changeImg} /></Link>
+      <Link to="/">
+        <MainLogo changeImg={changeImg} />
+      </Link>
       <Title>2023 커비의 당근심기</Title>
-      <NickName><p>{userInfo[0] === undefined ? null : userInfo[0].field}</p></NickName>
+        {userInfo[0] === undefined ? null : (
+          <Button onClick={logoutHandler}>LOGOUT<FontAwesomeIcon icon={faRightFromBracket} /></Button>
+        )}
     </HeaderContainer>
-
-
   );
 };
 
