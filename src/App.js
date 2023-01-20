afb814de86bfcedf73d7f798004e48fc988dd363
yjyaang <topbuty97@gmail.com>
2023-01-20 17:54:44 +0900

    feat: 배포 전 router 관련 설정

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
   }, [])
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className='app-wrap'>
         <GlobalStyle />
         <Header userInfo={userInfo}/>
